44225c35a61db68be3f952de27a49c0936aa557f
Daniel <gendik38@gmail.com>
2023-04-10 15:39:00 +0200

    add state for isGamestarted

--- src/components/Mainbanner.tsx
+++ src/components/Mainbanner.tsx
@@ -1,18 +1,17 @@
-import React from "react"
-import papericon from "../../images/icon-paper.svg"
+import React,{useState} from "react"
 import { Rock } from "./weapons/Rock"
 import { Scissors } from "./weapons/Scissors"
 import { Paper } from "./weapons/Paper"
 
 const Mainbanner = () => {
+  const [isGameStarted,setIsGameStarted] = useState<boolean>(false)
+
   return (
-    <main className="main-icons-ctn flex w-full min-h-[500px] justify-center flex-col items-center">
+    <main className="main-icons-ctn w-full min-h-[500px] justify-center flex-col items-center"style={{display:!isGameStarted? "flex" : "none"}}>
       <h1 className="text-white text-5xl font-bold mb-10">Pick your weapon</h1>
       <div className="icons w-[600px] flex items-center justify-center gap-20 flex-wrap bg-[url(../../images/bg-triangle.svg)] bg-no-repeat bg-center">
         <Paper />
-
         <Scissors />
-
         <Rock />
       </div>
     </main>
--- src/components/Rulesbtn.tsx
+++ src/components/Rulesbtn.tsx
@@ -2,10 +2,10 @@ import React from "react"
 
 const Rulesbtn = () => {
   return (
-    <div className="button-ctn flex justify-center w-full">
+    <div className="button-ctn flex justify-center w-full fixed">
       <button
         type="button"
-        className="text-white  font-normal border-2 mt-10 ml-52 lg:ml-[500px] px-7 py-2 rounded-md"
+        className="text-white  font-normal border-2 mt-10 ml-52 lg:ml-[500px] px-7 py-2 rounded-md "
       >
         Rules
       </button>
